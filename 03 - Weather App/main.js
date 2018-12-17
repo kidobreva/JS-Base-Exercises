231786c46429db863e654fc70f116db15f7eb0be
SCALEFOCUS\kremena.dobreva <kremena.dobreva@scalefocus.com>
2018-12-17 16:33:25 +0200

    Add additional info

--- 03 - Weather App/main.js
+++ 03 - Weather App/main.js
@@ -11,22 +11,40 @@ $('form').submit((e) => {
     })
 
     $('#result').empty();
-    function getData(res) {        
+    function getData(res) { 
+        console.log(res);       
         for (let i = 0; i < res.list.length; i += 8) {
-            let day = $('<div>').addClass('days');
+            let day = $('<div>').addClass('days').attr('id', 'div_' + (i + 1));
             let cityName = $('<h3>').html(res.city.name + ' ' + res.city.country);
             let date = $('<p>').html((res.list[i].dt_txt).slice(0, 10));
-            let currentTemp = $('<p>').html(Math.round(res.list[i].main.temp));
-            let degreeIcon = $('<img src="https://image.flaticon.com/icons/png/512/88/88692.png">').addClass('degreeIcon');
-            currentTemp.append(degreeIcon);
+            const degreeIcon = $('<img src="https://image.flaticon.com/icons/png/512/88/88692.png">').addClass('degreeIcon');
+            let currentTemp = $('<p>').html(Math.round(res.list[i].main.temp)).append(degreeIcon);
             let weatherDescription = $('<p>').html(res.list[i].weather[0].description);
             let icon = $('<img src="http://openweathermap.org/img/w/' + res.list[i].weather[0].icon + '.png"/>');
 
             day.append(cityName, date, currentTemp, weatherDescription, icon);
             $('#result').append(day);
-        }
+
+            let addData = $('<div>').addClass('addData');
+            let minTemp = $('<p>').html('Min: ' + Math.round(res.list[i].main.temp_min));
+            let maxTemp = $('<p>').html('Max: ' + Math.round(res.list[i].main.temp_max));
+            let tempIcon = $('<img src="https://image.flaticon.com/icons/png/512/88/88692.png">').addClass('degreeIcon');
+            tempIcon.appendTo(maxTemp, minTemp);
+            let clouds = $('<p>').html('Clouds: ' + res.list[i].clouds.all + '%');
+            let wind = $('<p>').html('Wind: ' + res.list[i].wind.speed);
+            let humidity = $('<p>').html('Humidity: ' + res.list[i].main.humidity + '%');
+            let pressure = $('<p>').html('Pressure: ' + res.list[i].main.pressure);
+
+            addData.append(minTemp, maxTemp, clouds, wind, humidity, pressure);
+            day.append(addData);
+
+            addData.hide();
+        }     
         
-    }
+        $('.days').click(function () {
+           $(this).find($('.addData')).toggle();
+        })
+    }    
 
     function displayError(err) {
         console.log(err);
